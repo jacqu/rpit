7ac777c00a8b1784d0a5e8890f0a1478abd51266
Jacques Gangloff <jacques.gangloff@gmail.com>
2016-11-28 13:10:59 +0100

    Delete init_fun.m

--- demos/Dynamixel_test/XM430/init_fun.m
+++
@@ -1,8 +0,0 @@
-% Called before Simulink starts
-
-if exist('last_positions.mat', 'file') == 2
-  load last_positions;
-  init_positions = mod( last_positions, 4096 );
-else
-  init_positions = 0;
-end
