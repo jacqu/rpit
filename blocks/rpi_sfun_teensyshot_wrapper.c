ef16984bd042fea5c30aaf7f582215c4bab9aed2
Jacques Gangloff <jacques.gangloff@gmail.com>
2019-06-26 12:52:02 +0200

    Add teensyshot block

---
+++ blocks/rpi_sfun_teensyshot_wrapper.c
@@ -0,0 +1,83 @@
+
+/*
+ * Include Files
+ *
+ */
+#if defined(MATLAB_MEX_FILE)
+#include "tmwtypes.h"
+#include "simstruc_types.h"
+#else
+#include "rtwtypes.h"
+#endif
+
+
+
+/* %%%-SFUNWIZ_wrapper_includes_Changes_BEGIN --- EDIT HERE TO _END */
+#include <math.h>
+/* %%%-SFUNWIZ_wrapper_includes_Changes_END --- EDIT HERE TO _BEGIN */
+#define u_width 1
+#define y_width 1
+
+/*
+ * Create external references here.  
+ *
+ */
+/* %%%-SFUNWIZ_wrapper_externs_Changes_BEGIN --- EDIT HERE TO _END */
+/* External function declarations */
+/* %%%-SFUNWIZ_wrapper_externs_Changes_END --- EDIT HERE TO _BEGIN */
+
+/*
+ * Output function
+ *
+ */
+void rpi_sfun_teensyshot_Outputs_wrapper(const real_T *u1,
+			const real_T *u2,
+			const real_T *u3,
+			const real_T *u4,
+			const real_T *u5,
+			const real_T *u6,
+			real_T *y1,
+			real_T *y2,
+			real_T *y3,
+			real_T *y4,
+			real_T *y5,
+			real_T *y6,
+			const real_T *rpi_Ts, const int_T p_width0,
+			const real_T *P1, const int_T p_width1,
+			const real_T *I1, const int_T p_width2,
+			const real_T *D1, const int_T p_width3,
+			const real_T *f1, const int_T p_width4,
+			const real_T *P2, const int_T p_width5,
+			const real_T *I2, const int_T p_width6,
+			const real_T *D2, const int_T p_width7,
+			const real_T *f2, const int_T p_width8,
+			const real_T *P3, const int_T p_width9,
+			const real_T *I3, const int_T p_width10,
+			const real_T *D3, const int_T p_width11,
+			const real_T *f3, const int_T p_width12,
+			const real_T *P4, const int_T p_width13,
+			const real_T *I4, const int_T p_width14,
+			const real_T *D4, const int_T p_width15,
+			const real_T *f4, const int_T p_width16,
+			const real_T *P5, const int_T p_width17,
+			const real_T *I5, const int_T p_width18,
+			const real_T *D5, const int_T p_width19,
+			const real_T *f5, const int_T p_width20,
+			const real_T *P6, const int_T p_width21,
+			const real_T *I6, const int_T p_width22,
+			const real_T *D6, const int_T p_width23,
+			const real_T *f6, const int_T p_width24,
+			const uint32_T *Port, const int_T p_width25)
+{
+/* %%%-SFUNWIZ_wrapper_Outputs_Changes_BEGIN --- EDIT HERE TO _END */
+/* This sample sets the output equal to the input
+      y0[0] = u0[0]; 
+ For complex signals use: y0[0].re = u0[0].re; 
+      y0[0].im = u0[0].im;
+      y1[0].re = u1[0].re;
+      y1[0].im = u1[0].im;
+ */
+/* %%%-SFUNWIZ_wrapper_Outputs_Changes_END --- EDIT HERE TO _BEGIN */
+}
+
+
